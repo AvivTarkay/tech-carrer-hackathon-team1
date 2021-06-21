fff4115cb6950fe7d44e6b00eabc4d64cb26e6e3
AvivTarkay <61126933+AvivTatkay@users.noreply.github.com>
2021-06-21 19:20:34 +0300

    add scripts to index

--- client/src/AdminRouter.tsx
+++ client/src/AdminRouter.tsx
@@ -1,76 +1,65 @@
-import React from 'react';
+import React from "react";
 //Admin page
-import AdminPage from './pages/admin-page'
+import AdminPage from "./pages/admin-page";
 //Home page
-import Home from './pages/home-page/Home'
+import Home from "./pages/home-page/Home";
 //HR page
-import Student from './pages/student-page/Student'
-import SpecificStudent from './components/specificStudent/SpecificStudent'  
-import HrRegisterForm from './pages/hr-page/Hr-Register'
+import Student from "./pages/student-page/Student";
+import SpecificStudent from "./components/specificStudent/SpecificStudent";
+import HrRegisterForm from "./pages/hr-page/Hr-Register";
+import LinkdIn from "./components/job-cards/LinkdIn";
 
-import LinkdIn from './components/job-cards/LinkdIn'
+import JobCards from "./pages/CompanyZone/JobCards";
+import StuSteps from "./components/steps/StuSteps";
+import StudentPersonal from "./pages/personal-page/StudentPersonal";
+import JobEditPage from "./pages/CompanyZone/JobEditPage";
 
-import JobCards from './pages/CompanyZone/JobCards'
-import StuSteps from './components/steps/StuSteps'
-import StudentPersonal from './pages/personal-page/StudentPersonal'
-import JobEditPage from './pages/CompanyZone/JobEditPage'
-
-
-
-import {
-  Switch,
-  Route
-} from "react-router-dom";
-import AddJob from './pages/CompanyZone/AddJob';
-
+import { Switch, Route } from "react-router-dom";
+import AddJob from "./pages/CompanyZone/AddJob";
+import App from "./pages/CompanyZone/smartJobs/App";
 
 function AdminRouter() {
-
- 
-
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/hr">
-        <JobCards />
-      </Route>
-      <Route path="/JobEditPage">
-        <JobEditPage/>
-      </Route>
-      <Route path="/addNewJob">
-        <AddJob/>
-      </Route>
-      <Route path="/signUp/hr/:token/:id">
-       <HrRegisterForm/>
-      </Route>
-      < Route path="/recruiter">
-        <LinkdIn />
-      </Route>
-     < Route path="/recruiter">
-            <LinkdIn/>
-          </Route>
-      <Route path="/signUp/company">
-      </Route>
-      <Route path="/student">
-        <Student />
-      </Route>
-      <Route path="/studentPage/:id">
-        <SpecificStudent />
-      </Route>
-      <Route path="/signUp/student/:token/:id">
-       <StuSteps/>
-      </Route>
-      <Route path="/admin">
-        <AdminPage />
-      </Route>
-      <Route path="/privatestudent">
-        <StudentPersonal />
-      </Route>
-    </Switch>
-  )
-
+	return (
+		<Switch>
+			<Route exact path="/">
+				<Home />
+			</Route>
+			<Route path="/hr">
+				<App />
+			</Route>
+			<Route path="/JobEditPage">
+				<JobEditPage />
+			</Route>
+			<Route path="/addNewJob">
+				<AddJob />
+			</Route>
+			<Route path="/signUp/hr/:token/:id">
+				<HrRegisterForm />
+			</Route>
+			<Route path="/recruiter">
+				<LinkdIn />
+			</Route>
+			<Route path="/recruiter">
+				<LinkdIn />
+			</Route>
+			<Route path="/signUp/company"></Route>
+			<Route path="/student">
+				<Student />
+			</Route>
+			<Route path="/studentPage/:id">
+				<SpecificStudent />
+			</Route>
+			<Route path="/signUp/student/:token/:id">
+				<StuSteps />
+			</Route>
+			<Route path="/admin">
+				<AdminPage />
+			</Route>
+			<Route path="/privatestudent">
+				<StudentPersonal />
+			</Route>
+		</Switch>
+	);
 }
 
 export default AdminRouter;
